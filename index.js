5955be43bbb114b4ac26e63ca63acb7ccf4bbae3
munir79 <jakirhossainmunir79@gmail.com>
2024-09-06 14:00:28 +0600

    connection and get all data

---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors=require('cors');
+const app = express();
+const port = process.env.PORT ||5000;
+
+
+
+
+
+//furniflex
+//EfbyrPs4M2jlQj6f
+
+
+//Middelware
+app.use(cors());
+app.use(express.json() );
+
+
+
+const uri = "mongodb+srv://furniflex:EfbyrPs4M2jlQj6f@cluster0.semifcf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("Furniflex");
+    const ChairCollection = database.collection("allcahir");
+
+    app.get('/chair',async(req,res)=>{
+        const cursor=ChairCollection.find();
+        const result=await cursor.toArray();
+        res.send(result);
+      })
+  
+
+    
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+   // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send(" simple crud is running");
+})
+
+
+app.listen(port ,()=>{
+    console.log(`simple crud running on ,${port}`);
+} )
